4273d48265837db4a1a9114966fdff5d13bd119f
jad issa <progressweb123@gmail.com>
2024-04-23 22:26:35 +0300

    argis-project

---
+++ learningasp/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using learningasp.Models;
+using learningasp.Data;
+using Microsoft.EntityFrameworkCore; // Make sure to add this using statement
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    private readonly Dbcontext _dbContext; // Change DbContext to Dbcontext
+
+    public UserController(Dbcontext dbContext) // Change DbContext to Dbcontext
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpPost("authenticate")]
+    public async Task<IActionResult> Authenticate([FromBody] User userObj)
+    {
+        try
+        {
+            if (userObj == null)
+            {
+                return BadRequest();
+            }
+
+            var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Username == userObj.Username && u.Password == userObj.Password);
+
+            if (user != null)
+            {
+                return Ok(new { user });
+            }
+            
+            else
+            {
+                return BadRequest(new {Message="user not found "});
+            }
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while processing your request.");
+        }
+    }
+
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] User userObj)
+    {
+        if (userObj == null)
+        {
+            return BadRequest();
+        }
+        await _dbContext.Users.AddAsync(userObj);
+        await _dbContext.SaveChangesAsync();
+        return Ok(new { Message= "user Registred" });
+
+    }
+}
---
+++ learningasp/Data/Dbcontext.cs
@@ -0,0 +1,22 @@
+﻿using learningasp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace learningasp.Data
+{
+    public class Dbcontext: DbContext
+    {
+        public  Dbcontext(DbContextOptions<Dbcontext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("users");       
+        }
+
+
+    }
+}
